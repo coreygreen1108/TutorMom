fd85706206319fba3c88931206925ae1807b8024
coreygreen1108 <coreygreen1108@gmail.com>
2016-06-16 16:33:40 -0400

    update

--- background.js
+++ background.js
@@ -1,3 +1,27 @@
-document.addEventListener('DOMContentLoaded', function(){
-	alert('hi');
-})
+// document.addEventListener('DOMContentLoaded', function(){
+// 		chrome.tabs.update(tab.id, {
+// 			url: "http://www.zombo.com"
+// 		})	
+// })
+
+// chrome.tabs.onCreated.addListener(function(tab, changeInfo){
+// 	chrome.tabs.update(tab.id, {
+// 		url: "http://www.zombo.com"
+// 	})
+// })
+
+// window.Geoff = true; 
+
+// function modifyGeoff(){
+// 	window.Geoff = !window.Geoff; 
+// }
+// setInterval(function(){
+// 	if(window.Geoff){
+// 		chrome.tabs.getCurrent(function(tab, info){
+// 			console.log(window.Geoff);
+// 			chrome.tabs.update(tab, {
+// 				url: "background.html"
+// 			})	
+// 		})
+// 	}
+// },3000)
