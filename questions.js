94ab8b422de181da6ce4493b2f9b8996383541d7
coreygreen1108 <coreygreen1108@gmail.com>
2016-06-19 18:21:33 -0400

    updated at store 621

--- questions.js
+++ questions.js
@@ -1,18 +1,21 @@
 $('document').ready(function(){
-	var questions = [['math', 'What is 2+2?', '4', 2, 3, 1],
-	['misc', 'What is Geoff\'s social security number?', '1', 2, 3, 4],
-	['science', 'What is the part of the brain thought responsible for memory?', 'hippocampus'],
-	['history', 'Who was the first president of the United States?', 'George Washington'],
-	['math', 'Solve this equation 4x + 3 = 19', '4'],
-	['science', 'What can you give a parkinson\'s patient to stop the tremors?', 'l-dopa'],
-	['misc', 'How high a building can go in NYC is based off this term', 'air rights'],
+	var questions = [
+	['math', '1', 'What is 2+2?', '4'],
+	['misc', '1', 'What is Geoff\'s social security number?','1'],
+	['science', '12+', 'What is the part of the brain thought responsible for memory?', 'hippocampus'],
+	['history', '2', 'Who was the first president of the United States?', 'George Washington'],
+	['math', '7', 'Solve this equation 4x + 3 = 19', '4'],
+	['science', '12+', 'What can you give a parkinson\'s patient to stop the tremors?', 'l-dopa'],
+	['misc', '12+', 'How high a an architect can build in NYC is known as what term', 'air rights'],
 	];
 
 	var questionNum = Math.floor(Math.random() * questions.length); 
 	var currentQuestion = questions[questionNum];
 	var category = currentQuestion[0];
+	var difficulty = currentQuestion[1];
 	$('#category').text("Category: " + category.toUpperCase());
-	$('#question').text(currentQuestion[1]);
+	$('#difficulty').text("Difficulty: " + difficulty);
+	$('#question').text(currentQuestion[2]);
 
 	$('.submitQuestion').on('click', submit);
 
@@ -22,7 +25,7 @@ $('document').ready(function(){
 
 	function submit () {
 		var answer = $('#answer').val();
-		if(answer.toLowerCase() == currentQuestion[2].toLowerCase()){
+		if(answer.toLowerCase() == currentQuestion[3].toLowerCase()){
 			chrome.mathDone = true;
 			chrome.runtime.sendMessage('dlfbaefmaboanbdoficjjdcpcnjikjba', {message: 'math done!'});
 		}
